bd05abc501490806820609e8d6dc40bfc7eb0116
j842 <j@842.be>
2016-08-23 20:00:50 +1200

    Tidying

--- drunner/service.lua
+++ drunner/service.lua
@@ -4,7 +4,6 @@ function drunner_setup()
 -- addconfig(NAME, DESCRIPTION, DEFAULT VALUE, TYPE, REQUIRED)
 -- addvolume(NAME, [BACKUP], [EXTERNAL])
 -- addcontainer(NAME)
-   addcontainer("drunner/minimalexample")  -- First one must always be this container.
 end
 
 -- everything past here are functions that can be run from the commandline,
