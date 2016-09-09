ea74b50895b550cf3cd3157f2bce0a4db4acde71
j842 <j@842.be>
2016-09-09 22:34:20 +1200

    More help

--- drunner/service.lua
+++ drunner/service.lua
@@ -1,9 +1,11 @@
--- drunner service configuration for helloworld
+-- drunner service configuration for minimalexample
 
 function drunner_setup()
--- addconfig(NAME, DESCRIPTION, DEFAULT VALUE, TYPE, REQUIRED)
+-- addconfig(NAME, DESCRIPTION, DEFAULT VALUE, TYPE, REQUIRED, USERSETTABLE)
 -- addvolume(NAME, [BACKUP], [EXTERNAL])
--- addcontainer(NAME)
+-- addcontainer(NAME, [RUNASROOT])
+-- addproxy(VIRTUAL_HOST,HTTP_PORT,HTTPS_PORT)
+-- addcron(OFFSETMINUTES, REPEATMINUTES, FUNCTIONNAME)
 end
 
 -- everything past here are functions that can be run from the commandline,
